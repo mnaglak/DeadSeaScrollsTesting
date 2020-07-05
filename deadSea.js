2558ce1573e2f72f73af0a7ca5cf4e78f8f96d04
mnaglak <mnaglak@umich.edu>
2020-07-05 17:49:49 -0400

    two types of movement

--- deadSea.js
+++ deadSea.js
@@ -46,9 +46,65 @@ L.control.test1 = function(opts) {
 }		
 		
 L.control.test1({ position: 'bottomleft' }).addTo(map);
+
 		
-	var marker = L.marker([48.86, 2.33], {draggable: true}).addTo(map);	
-/*	map.whenReady(function() {
-  // By default, 'img' will be placed centered on the map view specified above
-  img = L.distortableImageOverlay('DSS_stiched.jpg').addTo(map);
-});*/
+var marker = L.marker([48.86, 2.33], {draggable: true}).addTo(map);	
+
+
+
+var poly1 = [
+  [48.86, 2.33],
+  [48.92, 2.33],
+  [48.92, 2.43],
+  [48.86, 2.43]
+];
+
+var pg = new L.Polygon([poly1], {
+  draggable: true
+}).addTo(map);
+
+var imageBounds = L.latLngBounds([
+  poly1[0],
+  poly1[2]
+]);
+
+var overlay = new L.ImageOverlay("DSS_stitched.jpg", imageBounds, {
+  opacity: 1,
+  interactive: true
+}).addTo(map);
+
+
+pg.on('dragstart', function (e) {
+console.log(pg.getBounds());
+overlay.setBounds(pg.getBounds());
+});
+pg.on('drag', function (e) {
+console.log(pg.getBounds());
+overlay.setBounds(pg.getBounds());
+});
+pg.on('dragend', function (e) {
+console.log(pg.getBounds());
+overlay.setBounds(pg.getBounds());
+});
+
+var basemap = {};
+var overlays = {"Image1": overlay};
+L.control.layers(basemap, overlays).addTo(map);
+
+var opacityLayers = {
+	"Image1" : overlay};
+
+L.control.opacity(
+			opacityLayers, //the variable containing all the maps
+			{label: "<b>Opacity</b>", //the label for the box
+			position: 'topright',
+			collapsed: true} //if we want the opacity box to be collapsed or not. We can do the same thing for the control layers box if desired
+			).addTo(map);
+
+//Function to see map coordinates in console on click
+		map.on('click', function(e){
+			var coord = e.latlng;
+			var lat = coord.lat;
+			var lng = coord.lng;
+			console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+		});
